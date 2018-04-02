092a2a522026b9e60fc16016c503a32e0e503a6c
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2018-04-03 00:39:28 +0300

    MainActivityView is done
    started difficulty view, need to finish Main's logic

--- app/src/main/java/com/example/exoli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/exoli/myapplication/MainActivity.java
@@ -1,23 +1,46 @@
 package com.example.exoli.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtName;
     EditText edtAge;
+    TextView txtName;
+    TextView txtAge;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindUI();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToDifficultyActivity();
+            }
+        });
+    }
+
+    private void goToDifficultyActivity() {
+        Intent  intent = new Intent(this, DifficultyActivity.class);
+        startActivity(intent);
     }
 
     private void bindUI() {
         edtName = (EditText)findViewById(R.id.edtName);
         edtAge = (EditText)findViewById(R.id.edtAge);
+        txtName = (TextView)findViewById(R.id.txtName);
+        txtAge = (TextView)findViewById(R.id.txtAge);
+        btnSubmit = (Button)findViewById(R.id.btnSubmit);
     }
 }
